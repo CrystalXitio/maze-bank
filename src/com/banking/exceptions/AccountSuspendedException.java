393cc4ba0ab2451dad1d4d47199e029c256b0c04
Agrim <agrimarya18@gmail.com>
2026-04-19 18:50:56 +0530

    removing files

--- src/com/banking/exceptions/AccountSuspendedException.java
+++
@@ -1,7 +0,0 @@
-package com.banking.exceptions;
-
-public class AccountSuspendedException extends Exception {
-    public AccountSuspendedException(String message) {
-        super(message);
-    }
-}
